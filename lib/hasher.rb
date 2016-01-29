8d07ff244d1a9dd7b170993dad4a8cac807ca509
Alex Luecke <me@alexjl.com>
2016-01-28 17:13:16 -0800

    Hasher::mapper -> Hasher::map, Hasher::unmapper -> Hasher::unmap. Changed to a much more sane way of converting from hash to integer and vice-versa.

--- lib/hasher.rb
+++ lib/hasher.rb
@@ -1,46 +1,32 @@
 module Hasher
 
-  @base = 72
+  ALPHABET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split(//)
+  BASE = ALPHABET.length
 
-  def self.mapper num
-    case
-      when num >= 0 && num < 26    # a-z -> 0-25
-        return (num + 97).chr
-      when num >= 26 && num < 62   # A-Z -> 26-61
-        return (num - 26 + 65).chr
-      else                         # 0-9 -> 62-71
-        return (num - 62 + 48).chr
-    end
+  def self.map num
+    return ALPHABET[num]
   end
 
-  def self.unmapper ch
-    num = ch.ord
-    case
-      when num >= 97 && num < 123  # 0-25 -> a-z
-        return num - 97
-      when num >= 65 && num < 91   # 26-61 -> A-Z
-        return num - 65 + 26
-      else                         # 62-71 -> 0-9
-        return num - 48 + 62
-    end
+  def self.unmap ch
+    return ALPHABET.index(ch)
   end
 
   def self.unhash str
     sum=0
     str.chars.each_with_index { |ch, idx|
-      sum += self.unmapper(ch) * @base**idx
+      sum += self.unmap(ch) * BASE**idx
     }
     return sum
   end
 
   def self.hash num
 
-    return mapper(0) if num == 0
+    return map(0) if num == 0
 
     str = ''
     while num > 0 do
-      str += mapper(num % @base)
-      num = num / @base
+      str += self.map(num % BASE)
+      num = num / BASE
     end
     return str
   end
--- tests/test_hasher.rb
+++ tests/test_hasher.rb
@@ -4,22 +4,22 @@ require 'test/unit'
 class TestHasher < Test::Unit::TestCase
 
   def test_mappers
-    assert_equal('a', Hasher::mapper(0))
-    assert_equal('b', Hasher::mapper(1))
-    assert_equal('0', Hasher::mapper(62))
-    assert_equal('A', Hasher::mapper(26))
-    assert_equal('B', Hasher::mapper(27))
+    assert_equal('a', Hasher::map(0))
+    assert_equal('b', Hasher::map(1))
+    assert_equal('0', Hasher::map(52))
+    assert_equal('A', Hasher::map(26))
+    assert_equal('B', Hasher::map(27))
 
-    assert_equal(0, Hasher::unmapper('a'))
-    assert_equal(1, Hasher::unmapper('b'))
-    assert_equal(62, Hasher::unmapper('0'))
-    assert_equal(26, Hasher::unmapper('A'))
-    assert_equal(27, Hasher::unmapper('B'))
+    assert_equal(0, Hasher::unmap('a'))
+    assert_equal(1, Hasher::unmap('b'))
+    assert_equal(52, Hasher::unmap('0'))
+    assert_equal(26, Hasher::unmap('A'))
+    assert_equal(27, Hasher::unmap('B'))
   end
 
   def test_hash
-    assert_equal('Zb', Hasher::hash(123))
-    assert_equal(123, Hasher::unhash('Zb'))
+    assert_equal('9b', Hasher::hash(123))
+    assert_equal(123, Hasher::unhash('9b'))
   end
 
 end
